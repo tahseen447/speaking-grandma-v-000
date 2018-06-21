1c63b45a050d1acfc26da6def54d6182405fab8c
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-21 22:07:41 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -11,6 +11,7 @@ elsif(phrase == phrase.upcase())
 # and yells back
 
 # NO, NOT SINCE 1938!
+return "NO, NOT SINCE 1938!"
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'

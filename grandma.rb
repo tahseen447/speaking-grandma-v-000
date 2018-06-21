5a8a97d6ae04813a0fbc973fdea3d69c2217ee1b
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-21 22:08:23 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ return "NO, NOT SINCE 1938!"
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-elsif
+else
   return 'I LOVE YOU TOO PUMPKIN!'
 end
 

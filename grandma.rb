a10419f634273a41aa90fa147d1f374b44ae163d
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-21 22:05:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-
+if(phrase)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-
+return "HUH?! SPEAK UP, SONNY!"
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
@@ -12,7 +12,8 @@ def speak_to_grandma(phrase)
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
-if(phrase == "I LOVE YOU GRANDMA!")
+
+elsif
   return 'I LOVE YOU TOO PUMPKIN!'
 end
 

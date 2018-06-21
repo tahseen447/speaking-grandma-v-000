5a859014a029eebd3c16989625f1a1aeb08e9beb
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-21 22:11:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -9,12 +9,12 @@ return 'I LOVE YOU TOO PUMPKIN!'
 .
 #return "HUH?! SPEAK UP, SONNY!"
 
-elsif(phrase == phrase.upcase)
+#elsif(phrase == phrase.upcase)
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
-return "NO, NOT SINCE 1938!"
+#return "NO, NOT SINCE 1938!"
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
